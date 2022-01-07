820226e4a000c1407c0f7c499a414af1c335cb80
jhaptaal_ <maldpkdamle@gmail.com>
2022-01-07 10:30:02 -0500

    finished the Boomerang Count problem in JavaScript

--- 2021-12-20/BoomerangCount_Js/boomerangScript.js
+++ 2021-12-20/BoomerangCount_Js/boomerangScript.js
@@ -1,5 +1,6 @@
 var inputArray = [];
 
+//Resets the array specified by user
 function resetArray(event){
     event.preventDefault();
     console.log("reseting input array");
@@ -9,6 +10,7 @@ function resetArray(event){
 }
 document.getElementById("reset-array-btn").addEventListener("click", resetArray);
 
+//Allows user to add an integer element to the array
 function addElement(event){
     event.preventDefault();
     console.log("adding element");
@@ -24,3 +26,25 @@ function addElement(event){
     }
 }
 document.getElementById("input-array-element-btn").addEventListener("click", addElement);
+
+//Returns a boomerang count of a given array
+function boomerangCount(inArr){
+    let count = 0;
+    for(let i=0; i < inArr.length - 2; i++){
+        if(inArr[i] === inArr[i+2]){
+            if(inArr[i] === inArr[i+1]){
+                i++;
+            }else{
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+//Allows user to get boomerang count of the array
+function initiateCount(event){
+    event.preventDefault();
+    alert(boomerangCount(inputArray));
+}
+document.getElementById("boomerang-count-btn").addEventListener("click", initiateCount);
